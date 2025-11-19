2c5e14ab9df12880dd80c8a9954e12e432985807
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-19 13:00:54 +0100

    Create config.py

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = "smartbot_super_secret"
+    DATABASE_URL = "sqlite:///smartbot.db"
+
+    PAYPAL_CLIENT_ID = "YOUR_PAYPAL_ID"
+    PAYPAL_SECRET = "YOUR_PAYPAL_SECRET"

f874fd0cc7ea3c788dbc0d8a8bd522ac4dc20075
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-23 17:55:11 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,20 @@
-from flask import Flask
-from routes.auth import auth_bp
-from routes.billing import billing_bp
-from routes.payments import payments_bp
-from database import init_db
+from flask import Flask, render_template, session, redirect
+from auth import auth_bp
 
 app = Flask(__name__)
 app.secret_key = "SECRET_KEY"
 
-# تهيئة قاعدة البيانات (إنشاء جدول الأدمن + المستخدمين)
-init_db()
-
-# تسجيل البلوبربنت
 app.register_blueprint(auth_bp)
-app.register_blueprint(billing_bp)
-app.register_blueprint(payments_bp)
 
-@app.route('/')
-def home():
-    return "Server is running..."
-
-if __name__ == '__main__':
-    app.run(debug=True)
-@main_bp.route('/dashboard')
+@app.route('/dashboard')
 def dashboard():
     if 'user_id' not in session:
         return redirect('/login')
-
     return render_template('dashboard.html')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

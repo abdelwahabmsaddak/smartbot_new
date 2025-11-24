9738cba83510dd1a074cc81538ed2b0002a09f37
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-24 19:58:03 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ from auth import auth_bp
 from routes.admin import admin_bp
 from routes.profile import profile_bp
 from routes.usage import usage_bp
+from routes.chatbot import chatbot_bp
+app.register_blueprint(chatbot_bp)
 app.register_blueprint(usage_bp)
 app = Flask(__name__)
 app.secret_key = "SECRET_KEY"

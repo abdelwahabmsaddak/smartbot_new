39b6c59d69ebbd0432df44fa7f5b7dce93384680
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-11-26 22:00:14 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,7 @@ from flask import Flask, jsonify
 import requests
 import time
 from routes.auto_trading_pro import auto_trading_pro_bp
+from routes.screener import screener_bp
 app.register_blueprint(auto_trading_pro_bp)
 app = Flask(__name__)
 

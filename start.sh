effbc2226aaeadaebecab441b23b73c824eebb2a
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-12-10 20:42:33 +0100

    Change Gunicorn port to 10000

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-export PYTHONPATH=.
-export PORT=${PORT:-5000}
-
-# Initialize the database if needed
 python3 backend/init_db.py
-
-# Run Gunicorn server
-gunicorn -w 4 -b 0.0.0.0:$PORT backend.app:app
+gunicorn -w 4 -b 0.0.0.0:10000 backend.app:app

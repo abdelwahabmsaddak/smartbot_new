5d6b0797244135b1cd171568885b7e0e68105b28
abdelwahabmsaddak <abdelwahabmsaddak@gmail.com>
2025-12-25 20:44:51 +0100

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,10 +1,11 @@
 (() => {
-  const btn = document.getElementById('langBtn');
+  const btn = document.getElementById("langBtn");
   if(!btn) return;
+
   btn.onclick = () => {
-    const ar = document.documentElement.lang !== 'ar';
-    document.documentElement.lang = ar ? 'ar' : 'en';
-    document.documentElement.dir = ar ? 'rtl' : 'ltr';
-    btn.textContent = ar ? 'EN' : 'AR';
+    const ar = document.documentElement.lang !== "ar";
+    document.documentElement.lang = ar ? "ar" : "en";
+    document.documentElement.dir  = ar ? "rtl" : "ltr";
+    btn.textContent = ar ? "EN" : "AR";
   };
 })();
